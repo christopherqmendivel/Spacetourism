ee0f83eb788d1ebdd733921060526aac58f07108
Christopher Quiroz <cquirozmendivel@gmail.com>
2023-08-13 13:29:44 +0200

    crew interactive up

--- src/app/space-tourism/components/crew/crew.component.ts
+++ src/app/space-tourism/components/crew/crew.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../shared/services/data.service';
+import { Crew } from '../../interfaces/crew.interface';
 
 @Component({
   selector: 'app-crew',
@@ -7,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class CrewComponent {
 
+  public title: string = 'meet your crew';
+  public crewList: Crew[] = this.dataService.data$.value.crew;
+  public currentCrew: Crew = this.crewList[0];
+
+  constructor(private dataService: DataService ) {}
+
+  changeCurrent( id: number ) {
+    this.currentCrew = this.crewList[id];
+  }
 }
