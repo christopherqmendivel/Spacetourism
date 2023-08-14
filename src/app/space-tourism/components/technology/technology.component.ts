74b3482104027a83067c31145d8e2e3f4d9973f0
Christopher Quiroz <cquirozmendivel@gmail.com>
2023-08-14 14:51:10 +0200

    technology page up - interactive

--- src/app/space-tourism/components/technology/technology.component.ts
+++ src/app/space-tourism/components/technology/technology.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DataService } from '../../shared/services/data.service';
+import { Technology } from '../../interfaces/technology.interface';
 
 @Component({
   selector: 'app-technology',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class TechnologyComponent {
 
+  public title: string = 'space launch 101';
+
+  public techList: Technology[] = this.dataService.data$.value.technology;
+  public currentTech: Technology = this.techList[0];
+
+  constructor(private dataService: DataService ){}
+
+
+  changeCurrent(id: number ){
+    this.currentTech = this.techList[id];
+  }
 }
